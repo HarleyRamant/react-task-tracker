45793585856bab4cf0fe126590611b8a7632a1e4
Harley Ramant <31805718+HarleyRamant@users.noreply.github.com>
2021-04-25 17:09:27 +0200

    Added Button and Header component. Commiting to see changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Header from './components/Header'
 
-
 function App() {
-
-  const name = 'Harley'
 
   return (
     <div className="container">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,39 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+//You can deconstruct props =>     use "{ title }" instead of "props" so you can access "{title}" instead of "{props.title}"
+
+//Inline CSS
+const Header = (props) => {
+    const onClick = () => {
+        console.log('Click');
+    }
+
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className='header'>
+            <h1>{props.title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
+            <Button />
         </header>
     )
+}
+
+
+//When nothing is passed as argument of props, use the default value given below.
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+//Optionaly you can define what type the props properties have to be.
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+//To use inline css, add style={{color: 'red', backgroundColor: 'black'}} to the element.
+//To use this style, add style={headingStyle} to the element
+const headingStyle = {
+    color: 'red',
+    backgroundColor: 'black'
 }
 
 export default Header
